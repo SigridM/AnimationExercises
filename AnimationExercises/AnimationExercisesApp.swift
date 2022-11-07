cd68f08f9c3016eb3d5bca4c7a782ca60b3c52e8
SigridM <sigridm@selfhelpsoftware.com>
2022-11-07 12:13:37 -0500

    Initial Commit

---
+++ AnimationExercises/AnimationExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationExercisesApp.swift
+//  AnimationExercises
+//
+//  Created by Sigrid Mortensen on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
