16e5ef0c2469586ab716937eadbe0e129b23d21e
SigridM <sigridm@selfhelpsoftware.com>
2022-11-27 11:30:22 -0500

    Fixed my weird animation effect of the numbers sliding up and down on the very first animation after the simulator -- or app on my phone -- was launched. I did this by disabling all the animations on the card itself before applying the .onTapGesture animation. This finally worked. I also fixed a problem that was allowing cards to be dealt from the middle or bottom of the deck.

--- AnimationExercises/ContentView.swift
+++ AnimationExercises/ContentView.swift
@@ -15,14 +15,24 @@ struct ContentView: View {
     /// The set of cards that have currently been dealt onto the tableau
     @State private var tableau = Set<Int>()
     
-    private func transition(for cardNumber: Int) -> AnyTransition {
-        if isDealt(cardNumber) {
+    private func transition(if isDealt: Bool) -> AnyTransition {
+        if isDealt {
             return
                 .unrotate3DXTransition(fullRotation: CardConstants.initialRotation)
+                .combined(with: .move(edge: .bottom))
             
         } else {
             return
                 .rotate3DXTransition(fullRotation: CardConstants.initialRotation)
+                .combined(with: .move(edge: .top))
+        }
+    }
+    
+    private func colorIf(_ animated: Bool) -> Color {
+        if animated {
+            return .red
+        } else {
+            return CardConstants.textColor
         }
     }
     
@@ -33,36 +43,39 @@ struct ContentView: View {
     /// during transition animation.
     /// - Parameter index: Where the card is in the stack
     /// - Returns: a View encompasing both the rectangle background and the numbered-text foreground
-    func card(numbered index: Int, change: @escaping ()->()) -> some View {
+    func card(numbered index: Int, dealt: Bool = false, animate: Bool = true, change: @escaping ()->()) -> some View {
         VStack {
-            if !isDealt(index) {
+            if !dealt {
                 Spacer()
             }
-            ZStack {
-                RoundedRectangle(cornerRadius: CardConstants.cornerRadius)
-                    .foregroundColor(CardConstants.cardColor)
-                    .frame(width: CardConstants.cardWidth, height: CardConstants.cardHeight)
-                    .shadow(radius: CardConstants.shadowRadius,
-                            x: CardConstants.shadowOffset,
-                            y: CardConstants.shadowOffset)
-                Text("\(index + 1)")
-                    .font(CardConstants.cardFont)
-                    .foregroundColor(CardConstants.textColor)
-                    .frame(width: CardConstants.cardWidth,
-                           height: CardConstants.cardHeight,
-                           alignment: CardConstants.textAlignment)
-                
-            }
-            .zIndex(Double(index) * -1)
-            .onTapGesture {
-                withAnimation(dealAnimation(for: 0)) {
-                    change()
-                }
-            }
-            .matchedGeometryEffect(id: index, in: switchingNamespace)
-        }
-        .transition(transition(for: index))
+            VStack {
+                ZStack {
+                        RoundedRectangle(cornerRadius: CardConstants.cornerRadius)
+                            .foregroundColor(dealt ? .blue : CardConstants.cardColor)
+                            .frame(width: CardConstants.cardWidth, height: CardConstants.cardHeight)
+                            .shadow(radius: CardConstants.shadowRadius,
+                                    x: CardConstants.shadowOffset,
+                                    y: CardConstants.shadowOffset)
 
+                        Text("\(index + 1)")
+                            .font(CardConstants.cardFont)
+                            .foregroundColor(CardConstants.textColor)
+                }
+                .animation(nil, value: UUID())
+                .zIndex(Double(index + 1) * -1)
+                .onTapGesture {
+                    if animate {
+                        withAnimation(dealAnimation(for: 0)) {
+                            change()
+                        }
+                    } else {
+                        change()
+                    }
+                }
+                .matchedGeometryEffect(id: "card\(index)", in: switchingNamespace)
+            }
+        }
+        .transition(transition(if: dealt))
     }
     
     
@@ -76,10 +89,11 @@ struct ContentView: View {
                 VStack {
                     LazyVGrid(columns: [GridItem(.adaptive(minimum: CardConstants.cardWidth))]) {
                         ForEach(0..<CardConstants.totalCards, id: \.self) { index in
+                            let dealt = isDealt(index)
                             // only show the card in the LazyVGrid if it has been dealt
-                            if isDealt(index) {
+                            if dealt {
                                 VStack {
-                                    card(numbered: index) {
+                                    card(numbered: index, dealt: true) {
                                         reset()
                                     }
                                 }
@@ -107,9 +121,10 @@ struct ContentView: View {
         VStack {
             ZStack {
                 ForEach(0..<CardConstants.totalCards, id: \.self) { index in
-                    if !isDealt(index) {
-                        card(numbered: index) {
-                            deal(index, inGroupOf: 3)
+                    let dealt = isDealt(index)
+                    if !dealt {
+                        card(numbered: index, dealt: false, animate: false) {
+                            deal(min(index, lowestUndealtIndex()), inGroupOf: 3)
                         }
                         .verticallyStacked(at: Double(index), in: Double(CardConstants.totalCards))
                     }
@@ -123,9 +138,10 @@ struct ContentView: View {
     ///   - cardIndex: the position of the card within the deck
     /// - Returns: a spring Animation, delayed according to the card's position.
     private func dealAnimation(for cardIndex: Int) -> Animation {
-        print(cardIndex)
-        let delay = Double(cardIndex) * CardConstants.cumulativeDealDelay
-        return .spring(response: 1, dampingFraction: 1, blendDuration: 1).delay(delay)
+        let delay = (Double(cardIndex) ) * CardConstants.cumulativeDealDelay
+        return .spring(response: 1, dampingFraction: 1, blendDuration: 0).delay(delay)
+//                return .easeInOut.delay(delay)
+
     }
     
     /// Record that the card numbered index has been moved from the deck to the tableau
@@ -139,7 +155,7 @@ struct ContentView: View {
     private func deal(_ index: Int, inGroupOf dealSize: Int) {
         for i in index..<(index+dealSize) {
             if !isDealt(i) {
-                withAnimation(dealAnimation(for: i % dealSize)) {
+                let _ = withAnimation(dealAnimation(for: i % dealSize)) {
                     tableau.insert(i)
                 }
             }
@@ -151,6 +167,16 @@ struct ContentView: View {
     /// - Returns: a Bool: true if the card with that index is on the tableau, not in the deck
     private func isDealt(_ index: Int) -> Bool {
         tableau.contains(index)
+    }
+    
+    /// Answer the smallest index of the cards still in the deck (not on the tableau). This helps to keep us from
+    /// dealing from the middle or bottom of the deck if a lower card is tapped.
+    /// - Returns: an Int for the lowest card index
+    private func lowestUndealtIndex() -> Int {
+        if let max = tableau.max() {
+            return max + 1
+        }
+        return 0
     }
     
     /// Moves all the cards back into the deck and off the tableau.
